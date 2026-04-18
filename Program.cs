43fdf98db85e737de8281a916d8454cf141a6384
Muh Fauzan <mhmdpojan06@gmail.com>
2026-04-18 12:42:01 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Mahasiswa
+{
+    public string Nim { get; set; }
+    public string Nama { get; set; }
+    public float Ipk { get; set; }
+
+    public void Registrasi()
+    {
+        Console.WriteLine("Nama: " + Nama);
+        Console.WriteLine("NIM: " + Nim);
+        Console.WriteLine("IPK: " + Ipk);
+        Console.WriteLine("Telah melakukan registrasi\n");
+    }
+
+    public void IsiKrs()
+    {
+        Console.WriteLine(Nama + " sedang melakukan proses KRS\n");
+    }
+}
